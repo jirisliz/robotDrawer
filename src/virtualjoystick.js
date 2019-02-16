fc03275074cbad40c6a249b9bc7ec8b8c1a82fa7
jirisliz <rufius.sliz@gmail.com>
2019-02-16 13:08:56 +0100

    joystick color changed to gray

--- src/virtualjoystick.js
+++ src/virtualjoystick.js
@@ -2,7 +2,7 @@ var VirtualJoystick	= function(opts)
 {
 	opts			= opts			|| {};
 	this._container		= opts.container	|| document.body;
-	this._strokeStyle	= opts.strokeStyle	|| 'YELLOWGREEN';
+	this._strokeStyle	= opts.strokeStyle	|| 'GRAY';
 	this._stickEl		= opts.stickElement	|| this._buildJoystickStick();
 	this._baseEl		= opts.baseElement	|| this._buildJoystickBase();
 	this._mouseSupport	= opts.mouseSupport !== undefined ? opts.mouseSupport : false;
